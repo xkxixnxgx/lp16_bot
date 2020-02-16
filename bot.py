1461e04b18d9371616ee326e5e2335b19bfa5bd6
Yury_Borodich <spf.yb@mail.ru>
2020-02-16 20:51:45 +0300

    Внес испавления в код, доабвил функции логирования и эхо.

--- bot.py
+++ bot.py
@@ -1,9 +1,25 @@
-# Импортируем нужные компоненты, импорт обработчика команд
-from telegram.ext import Updater, CommandHandler
+# Импортируем нужные компоненты, импорт обработчика команд, обработчик текстовых сообщений
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Настройки прокси
-PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080','urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+# Будем записывать отчет о работе бота
+import logging
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log')
+
+# Функция, которая соединяется с платформой Telegram, "тело" нашего бота
+def main():
+    mybot = Updater("908634581:AAEZYDLx0qq4MIcbfdonViKeQgY6J3MsOpM", request_kwargs=PROXY)
+    # Команды в чате Telegram выглядят как "/команда".
+    # Научим бота реагировать на команду /start - она автоматически выполняется при подключении к боту.
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))    # Добавим новый handler в main()
+    mybot.start_polling()
+    mybot.idle()
 
 # Создадим функцию greet_user
 # Она будет вызываться,
@@ -13,15 +29,12 @@ def greet_user(bot, update):
     print(text)
     update.message.reply_text(text)
 
-# Функция, которая соединяется с платформой Telegram, "тело" нашего бота
-def main():
-    mybot = Updater("908634581:AAEZYDLx0qq4MIcbfdonViKeQgY6J3MsOpM", request_kwargs=PROXY)
-    # Команды в чате Telegram выглядят как "/команда".
-    # Научим бота реагировать на команду /start - она автоматически выполняется при подключении к боту.
-    dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
-    mybot.start_polling()
-    mybot.idle()
+# Добавим функцию, которая будет "отвечать" пользователю
+def talk_to_me(bot, update):
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
 
 # Вызываем функцию - эта строчка собственно запускает бота
 main()
+
