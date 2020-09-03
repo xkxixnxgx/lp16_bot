8e0e90209b0bb949f9380e346f52b97577ba4e33
Yury Borodich <xkxixnxgx@yandex.ru>
2020-09-03 07:58:34 +0300

    Отключены настройки proxy. Проверена работоспособность логирования. Проверены зависимости.

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # Импортируем нужные компоненты, импорт обработчика команд, обработчик текстовых сообщений
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import settings
+from settings import API_KEY
+
 # Будем записывать отчет о работе бота
 import logging
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
@@ -9,20 +10,19 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
 
 
 # Настройки прокси
-PROXY = {'proxy_url': settings.PROXY_URL,
-    'urllib3_proxy_kwargs': {
-        'username': settings.PROXY_USERNAME,
-        'password': settings.PROXY_PASSWORD
-    }
-}
+# PROXY = {'proxy_url': settings.PROXY_URL,
+#     'urllib3_proxy_kwargs': {
+#         'username': settings.PROXY_USERNAME,
+#         'password': settings.PROXY_PASSWORD
+#     }
+# }
 
 
 # Создадим функцию greet_user
 # Она будет вызываться,
 # когда пользователь в чате напишет /start вручную или подключится к боту в первый раз.
 def greet_user(update, context):
-    text = 'Привет, как дела?'
-    print(text)
+    text = 'Привет! Я бот-дразнилка.\nКак у тебя дела?'
     update.message.reply_text(text)
 
 
@@ -36,9 +36,9 @@ def talk_to_me(update, context):
 # Функция, которая соединяется с платформой Telegram, "тело" нашего бота
 if __name__ == "__main__":
     def main():
-        # для прокси
+        # для использования прокси
         # mybot = Updater(settings.API_KEY, request_kwargs=PROXY)
-        mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
+        mybot = Updater(API_KEY, use_context=True)
         # Команды в чате Telegram выглядят как "/команда".
         # Научим бота реагировать на команду /start - она автоматически выполняется при подключении к боту.
         dp = mybot.dispatcher
